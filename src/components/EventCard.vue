e35e96d025cd13833c0d6830963e58c163c50620
Shahid Sarker <shahid.sarker@gmail.com>
2023-11-11 13:08:23 -0500

    setup axios and eventservice

--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -10,7 +10,7 @@ defineProps({
 <template>
   <div class="event-card">
     <h2>{{ event.title }}</h2>
-    <span>@{{ event.time }} on {{ event.date }}</span>
+    <span>@ {{ event.time }} on {{ event.date }}</span>
   </div>
 </template>
 
